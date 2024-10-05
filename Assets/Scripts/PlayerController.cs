e501b88d02b07c70d1e5489a4c989f814d310f52
DaveyNavy <davidyzhu2@gmail.com>
2024-10-04 19:52:24 -0700

    Added room swaps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -16,5 +17,15 @@ public class PlayerController : MonoBehaviour
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
         transform.position = mousePosition;
+    }
+
+    void OnSwapRoomOne(InputValue value)
+    {
+        mainCamera.transform.position = new Vector3(0, 0, -10);
+    }
+
+    void OnSwapRoomTwo(InputValue value)
+    {
+        mainCamera.transform.position = new Vector3(20, 0, -10);
     }
 }
