e4b0a0fdd9b7957613ef25941b7e8a9811d0b6b9
Brian Lu <brianlu999@gmail.com>
2024-10-06 16:59:08 -0700

    added erratic movement

---
+++ Assets/Scripts/EnemyErratic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyErratic : MonoBehaviour
+{
+    [SerializeField] public float netSpeed;
+    [SerializeField] public float noiseSpeed;
+    [SerializeField] public Vector3 position;
+
+    Rigidbody2D rb;
+    float theta;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        theta = Random.Range(0, 2 * Mathf.PI);
+    }
+
+    void Update()
+    {
+        Vector3 velocity = position - transform.position;
+        velocity.Normalize();
+        velocity *= netSpeed;
+        theta += Random.Range(-0.1f, 0.1f);
+        velocity.x += noiseSpeed * Mathf.Cos(theta);
+        velocity.y += noiseSpeed * Mathf.Sin(theta);
+
+        transform.position += velocity * Time.deltaTime;
+
+        var vAngle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        rb.MoveRotation(vAngle - 90);
+    }
+}
